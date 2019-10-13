eb5e349f415dcab21e2c7df57fcfae7e53a21fe3
omaiyea <omaiyea@gmail.com>
2019-10-13 13:01:34 -0700

    update to use heroku server

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    NOTE_ENDPOINT: `http://localhost:8000/api/notes`,
-    FOLDER_ENDPOINT: `http://localhost:8000/api/folders`,
+    NOTE_ENDPOINT: `https://fathomless-scrubland-33893.herokuapp.com/api/notes`,
+    FOLDER_ENDPOINT: `https://fathomless-scrubland-33893.herokuapp.com/api/folders`,
 }
